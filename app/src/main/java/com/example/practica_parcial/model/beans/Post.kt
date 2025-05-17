f431c1bee5860ee61d1a2846019e2eb65b86b7e1
Al3xiel <gabriel280104casas@gmail.com>
2025-05-17 01:38:17 -0500

    feat(room): implemented annotations for room support.

--- app/src/main/java/com/example/practica_parcial/model/beans/Post.kt
+++ app/src/main/java/com/example/practica_parcial/model/beans/Post.kt
@@ -1,8 +1,17 @@
 package com.example.practica_parcial.model.beans
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "posts")
 data class Post (
-    var userId: Int,
+    @PrimaryKey
     var id: Int,
+    @ColumnInfo(name = "userId")
+    var userId: Int,
+    @ColumnInfo(name = "title")
     var title: String,
+    @ColumnInfo(name = "body")
     var body: String
 )
